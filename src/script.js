8f9329297ed771691e3fee5666ce5a99fde05b64
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-05-11 18:22:31 +0200

    groupwork

--- src/script.js
+++ src/script.js
@@ -4,10 +4,15 @@ const todo = {
     id: new Date().getTime(),
     task: userInputValue,
     done: false,
-        };
+};
+
 function addTask() {
- const str = document.getElementById("newTask").value
- const div = document.createElement("div")
- div.textContent = `${todo{1}}`;
- body.append(div)
-}
+    const str = document.getElementById("newTask").value
+    const div = document.createElement("div")
+    div.textContent = `${todo{1}}`;
+    body.append(div)
+
+};
+document.body.
+     
+ }
