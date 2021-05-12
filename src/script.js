f697bb50193d18a2e4fafdc0559e91b01d0f6107
ThomasBeckDresden <thomas.beck.dresden@gmail.com>
2021-05-12 16:43:04 +0200

    major changes!

--- src/script.js
+++ src/script.js
@@ -1,65 +1,55 @@
+const inputField = document.getElementById('addTask')
+const taskList = document.getElementById('taskList')
+
 let idCounter = 0
+
 const inputFocus = () => {
     const input = document.getElementById('addTask');
     input.focus()
 }
+
 inputFocus();
 
-const inputField = document.getElementById('addTask')
-console.log(inputField)
+
+
 inputField.addEventListener('keyup', (event) => {
 
     if (event.key === 'Enter') {
-        btnAdd.click();
-        console.log(event.key)
+        addTask()
+        
     }
 })
 function addTask() {
-    
-    const str = document.getElementById("addTask").value
-    //console.log(str)
-    const taskList = document.getElementById('taskList')
-    const newLi = document.createElement('li')
+    const str = inputField.value
+
     idCounter++
-    newLi.className='listItem'
-    newLi.textContent = `${str}`;
-    taskList.innerHTML += `<li class="listItem" id="${idCounter}">
+   
+    taskList.innerHTML += `<li class="listItem" id="${idCounter}" data-task-id=${idCounter}>
                                 <div class="row align-items-center">
                                 <div class="col-auto m-3"><input type="checkbox"></div>
                                 <div class="col taskName">${str}</div>
-                                <div class="col-auto"><input type="button" value="" class="btn deleteButton" id="btn${idCounter}"></div>
+                                <div class="col-auto"><input type="button" value="" class="btn deleteButton" data-delete></div>
                                 </div>
                             </li>`
-    btnDelete();
-    const inputField = document.getElementById('addTask')
+
+
+    const deleteButtons = document.querySelectorAll('[data-delete]')
+
+    for (let button of deleteButtons) {
+        button.addEventListener('click', deleteTask)
+    }
+
+
     inputField.value = ""
 };
 const btnAdd = document.getElementById('btnAdd') 
+
 btnAdd.addEventListener("click", addTask)
 
-function deleteTask(id) {
- console.log(id)
-const taskList = document.getElementById('taskList')
-console.log(taskList.childNodes[id])
-taskList.removeChild(taskList.childNodes[id])
-if(idCounter>=1){
-    idCounter--
-}
-}
-
-const btnDelete = function () {
-    
-        const lastChildNode = document.getElementById('taskList').lastChild.id
-        console.log(lastChildNode)
-        for(let i=1;i<=lastChildNode;i++){
-        document.getElementById(`btn${i}`).addEventListener('click', function () {
-        deleteTask(i)
-        })
-    }
-}
-
-
-
-
+function deleteTask() {
+    const taskContainer = event.currentTarget.closest('.listItem')
+    console.log(taskContainer.dataset)
+    console.log(taskContainer.dataset.taskId)
+    taskContainer.remove()
 
 
