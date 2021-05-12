41bdcf8509300c68f9f3a6e6dc339a7723728266
zapatacarlos <49616574+zapatacarlos@users.noreply.github.com>
2021-05-12 19:40:42 +0200

    Minor improvements
    Remove counter for addTask() and use getTime() method instead. // Use insertAdjacentHTML in addTask() to avoid use of innerHTML. // Change str variable name to userInputTask (Readability). // Add missing closing bracket in deleteTask() function.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
-const inputField = document.getElementById('addTask')
-const taskList = document.getElementById('taskList')
+const inputField = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
 
-let idCounter = 0
 
 const inputFocus = () => {
     const input = document.getElementById('addTask');
@@ -10,28 +9,30 @@ const inputFocus = () => {
 
 inputFocus();
 
-
-
 inputField.addEventListener('keyup', (event) => {
-
+    
     if (event.key === 'Enter') {
         addTask()
-        
     }
 })
+
 function addTask() {
-    const str = inputField.value
+    const userInputTask = inputField.value
 
-    idCounter++
-   
-    taskList.innerHTML += `<li class="listItem" id="${idCounter}" data-task-id=${idCounter}>
-                                <div class="row align-items-center">
+    // Create the id for the new task
+    newDate = new Date();
+    newTime = newDate.getTime();
+    newTaskId = newTime;
+
+    newListTemplate  = `<li class="listItem" id="${newTaskId}" data-task-id=${newTaskId}>
+                          <div class="row align-items-center">
                                 <div class="col-auto m-3"><input type="checkbox"></div>
-                                <div class="col taskName">${str}</div>
+                                <div class="col taskName">${userInputTask}</div>
                                 <div class="col-auto"><input type="button" value="" class="btn deleteButton" data-delete></div>
-                                </div>
-                            </li>`
-
+                           </div>
+                        </li>`
+    
+    taskList.insertAdjacentHTML('beforeend',newListTemplate);
 
     const deleteButtons = document.querySelectorAll('[data-delete]')
 
@@ -39,10 +40,10 @@ function addTask() {
         button.addEventListener('click', deleteTask)
     }
 
-
     inputField.value = ""
 };
-const btnAdd = document.getElementById('btnAdd') 
+
+const btnAdd = document.getElementById('btnAdd')
 
 btnAdd.addEventListener("click", addTask)
 
@@ -51,5 +52,5 @@ function deleteTask() {
     console.log(taskContainer.dataset)
     console.log(taskContainer.dataset.taskId)
     taskContainer.remove()
-
+};
 
