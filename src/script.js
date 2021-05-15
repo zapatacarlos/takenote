7c8269a1e0d7f269765f29e33082d1aebbfa62ed
zapatacarlos <49616574+zapatacarlos@users.noreply.github.com>
2021-05-15 13:58:24 +0200

    Add progress ring 1.0

--- src/script.js
+++ src/script.js
@@ -41,6 +41,8 @@ function addTask() {
 
   taskList.insertAdjacentHTML("afterbegin", newListTemplate);
 
+  pendingTasks++;
+
   const deleteButtons = document.querySelectorAll("[data-delete]");
 
   for (let button of deleteButtons) {
@@ -49,6 +51,7 @@ function addTask() {
   checkedItemFn();
   inputField.value = "";
 }
+
 function checkedItemFn() {
   const checkedboxes = document.querySelectorAll(".box");
 
@@ -69,7 +72,63 @@ btnAdd.addEventListener("click", addTask);
 
 function deleteTask() {
   const taskContainer = event.currentTarget.closest(".listItem");
-  console.log(taskContainer.dataset);
-  console.log(taskContainer.dataset.taskId);
+  // console.log(taskContainer.dataset);
+  // console.log(taskContainer.dataset.taskId);
   taskContainer.remove();
 }
+
+// Progress ring
+
+let pendingTasks = 100;
+let completedTasks = 18;
+
+function drawRingProgress(pendingTasks, completedTasks) {
+
+  var el = document.getElementById('graph'); // get canvas
+
+  let progress_percentage = Math.floor((completedTasks / (completedTasks + pendingTasks)) * 100);
+
+  var options = {
+    // percent:  el.getAttribute('data-percent') || 25,
+    percent: progress_percentage,
+    // size: 110,
+    size: el.getAttribute('data-size') || 220,
+    lineWidth: el.getAttribute('data-line') || 15,
+    rotate: el.getAttribute('data-rotate') || 0
+  }
+
+  var canvas = document.createElement('canvas');
+  var span = document.createElement('span');
+  span.textContent = options.percent + '%';
+
+  if (typeof (G_vmlCanvasManager) !== 'undefined') {
+    G_vmlCanvasManager.initElement(canvas);
+  }
+
+  var ctx = canvas.getContext('2d');
+  canvas.width = canvas.height = options.size;
+
+  el.appendChild(span);
+  el.appendChild(canvas);
+
+  ctx.translate(options.size / 2, options.size / 2); // change center
+  ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+  //imd = ctx.getImageData(0, 0, 240, 240);
+  var radius = (options.size - options.lineWidth) / 3.2;
+
+  var drawCircle = function (color, lineWidth, percent) {
+    percent = Math.min(Math.max(0, percent || 1), 1);
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+    ctx.strokeStyle = color;
+    ctx.lineCap = 'round'; // butt, round or square
+    ctx.lineWidth = lineWidth
+    ctx.stroke();
+  };
+
+  drawCircle('#efefef', options.lineWidth, 100 / 100);
+  drawCircle('#046582', options.lineWidth, options.percent / 100)
+}
+
+drawRingProgress(pendingTasks, completedTasks);
